e4871afc6fd419619e77815065c04e3479225718
Documenter.jl <documenter@juliadocs.github.io>
2024-06-05 11:00:51 +0000

    build based on 4c436e0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IsoTNS","category":"page"},{"location":"#IsoTNS","page":"Home","title":"IsoTNS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IsoTNS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IsoTNS]","category":"page"}]
+}
